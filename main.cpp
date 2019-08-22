197f47cd93f54042a86ae8f89314607de79fd73a
flt6 <1404262047@qq.com>
2019-08-22 14:36:28 +0800

    finnal
    修复大量漏洞

--- main.cpp
+++ main.cpp
@@ -23,26 +23,30 @@ int main(){
 		cin>>p.x>>p.y;
 		p.x--;p.y--;
 		if (p.x!=s.x&&p.y!=s.y||p.x>3||p.x<0||p.y>3||p.y<0){
-			cout<<"输入错误!";
+			cout<<"输入错误!"<<endl;
 			continue;
 		}
 		if (p.y==s.y){
-			plus=(p.x>s.x)?-1:1;
-			for (int x=s.x;x==p.x;x+=plus){
+			plus=(p.x>s.x)?1:-1;
+			for (int x=s.x;x!=p.x;x+=plus){
 				arr[p.y][x]=arr[p.y][x+plus];
 			}
 		}
 		else if (p.x==s.x){
-			plus=(p.y>s.y)?-1:1;
-			for (int y=s.y;y==p.y;y+=plus){
-				arr[p.x][y]=arr[p.x][y+plus];
+			plus=(p.y>s.y)?1:-1;
+			for (int y=s.y;y!=p.y;y+=plus){
+				arr[y][p.x]=arr[y+plus][p.x];
 			}
 		}
+		arr[p.y][p.x] = 0;
+		s.x=p.x;s.y=p.y;
 	}
+	printf("成功了！\n");
 	return 0;
 }
 
 point init(){
+	printf("init\n");
 	srand(time(NULL));
 	point p;
 	for (int i=0;i<n;i++){
@@ -50,12 +54,14 @@ point init(){
 			arr[j][i]=0;
 		}
 	}
+	printf("finish 0\n");
 	for (int i=1;i<=n*n-1;i++){
 		do{
 			p.x=rand()%n;
 			p.y=rand()%n;
+			printf("do while\n");
 		}
-		while (arr[p.y][p.x]==0);
+		while (arr[p.y][p.x]!=0);
 		arr[p.y][p.x]=i;
 	}
 	for (p.x=0;p.x<n;p.x++){
@@ -67,9 +73,9 @@ point init(){
 
 void show(){
 	point p;
-	for (p.x=0;p.x<n;p.x++){
-		for (p.y=0;p.y<n;p.y++){
-			cout<<arr[p.y][p.x]<<" ";
+	for (p.y=0;p.y<n;p.y++){
+		for (p.x=0;p.x<n;p.x++){
+			printf("%3d ",arr[p.y][p.x]);
 		}
 		cout<<endl;
 	}
@@ -77,12 +83,12 @@ void show(){
 
 bool check(){
 	point p;
-	int i=1;
+	int i=0;
 	for (p.y=0;p.y<n;p.y++){
 		for (p.x=0;p.x<n;p.x++){
+			if (i==n*n-1) return true;
+			else i++;
 			if (arr[p.y][p.x]!=i) return false;
-			i++;
 		}
 	}
-	return true;
 }
